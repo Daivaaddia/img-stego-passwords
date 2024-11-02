d0d7407f754e6937aa4cf928a12b9b179f608be7
Daiva Krisnu Faisa Addia <z5508443@ad.unsw.edu.au>
2024-11-02 21:49:20 +1100

    Add encoder header file

---
+++ encoder.h
@@ -0,0 +1,17 @@
+#include <string>
+#ifndef ENCODER_H
+#define ENCODER_H
+
+
+void encodePlaintext(char *inputFile, unsigned char *message, int msgLen, char *outputFile);
+std::string decodePlaintext(char *inputFile);
+void encodeAES(char *inputFile, unsigned char *message, int msgLen, char *outputFile, char *outputKeyFile);
+std::string decodeAES(char *inputFile, char *inputKeyFile);
+
+const int ENCODE = 0;
+const int DECODE = 1;
+
+const int PLAINTEXT_MODE = 0;
+const int AES_MODE = 1;
+
+#endif
